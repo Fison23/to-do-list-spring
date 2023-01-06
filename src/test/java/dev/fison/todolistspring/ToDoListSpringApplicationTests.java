cd611c0978e3bd23e85545bf7860c8b15ffd1548
Vladyslav Fisochenko <Fison23@gmail.com>
2023-01-06 09:36:53 +0200

    Initial commit

---
+++ src/test/java/dev/fison/todolistspring/ToDoListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fison.todolistspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
